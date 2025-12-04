1718a631048836b06c4569e8d176176fabf48832
jiteshseth60-boop <jiteshseth60@gmail.com>
2025-12-04 14:07:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,12 +6,7 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
-});
+const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/ping', (req, res) => res.send('OK'));
